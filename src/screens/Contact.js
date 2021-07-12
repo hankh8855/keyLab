501aaf47460b999ada7c71b5d8d017c42613df6a
hankh8855 <hankh8855@gmail.com>
2021-07-13 02:49:27 +0900

    contact 페이지 랜덤유저 리스트 추가

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, SafeAreaView} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import screenStyles from "../styles/ScreenStyles";
+import axios from "axios";
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 function Contact() {
+  const [users, setUsers] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  useEffect(()=>{
+    axios.get(`https://randomuser.me/api/?results=15`)
+    .then(res=>res.data)
+    .then(({results})=>{
+      setUsers(results);
+    });
+  },[]);
+
+  const renderUser = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.userItem} key={item.login.uuid}>
+        <Image 
+          style={styles.userImage}
+          source={{
+            uri: item.picture.thumbnail,
+          }}
+        />
+        <Text style={styles.userName}>{item.name.first} {item.name.last}</Text>
+      </TouchableOpacity>
+    )
+  };
   return (
-    <View style={screenStyles.container}>
-      <Text style={screenStyles.text}>contact</Text>
-    </View>
+    <SafeAreaView style={screenStyles.container} forceInset={{ top: 'never' }}>
+      <FlatList 
+        data={users}
+        renderItem={renderUser}
+        keyExtractor={(item)=>item.login.uuid}
+        // extraData={selectedId}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -26,4 +56,21 @@ export default function StackScreen() {
 }
 
 const styles = StyleSheet.create({
+  userItem: {
+    flex:1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 70,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+  },  
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius:10,
+  },
+  userName: {
+    paddingLeft: 20,
+    color: 'yellow',
+  }
 });
